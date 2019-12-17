8e256379bb8d7f9a70349fbe725f191485d36628
ayxan386 <ayxanpro@gmail.com>
2019-12-17 23:26:25 +0400

    -- implemented the Product repository

--- src/main/java/com/shopers/repositories/ProductRepo.java
+++ src/main/java/com/shopers/repositories/ProductRepo.java
@@ -1,4 +1,10 @@
 package com.shopers.repositories;
 
-public interface ProductRepo {
+import com.shopers.entities.Product;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepo extends CrudRepository<Product, Integer> {
+
 }
