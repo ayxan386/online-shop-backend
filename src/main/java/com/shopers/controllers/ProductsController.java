e7b5404cf94b3a39823cc35eb4e49071d0fa9648
ayxan386 <ayxanpro@gmail.com>
2019-12-18 16:10:15 +0400

    -- implemented "/" GET -- implemented "/" POST -- implemented findAll service method -- implemented saveAll service method

--- src/main/java/com/shopers/controllers/ProductsController.java
+++ src/main/java/com/shopers/controllers/ProductsController.java
@@ -1,4 +1,31 @@
 package com.shopers.controllers;
 
+import com.shopers.entities.Product;
+import com.shopers.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products/")
 public class ProductsController {
+  private final ProductService productService;
+
+  @Autowired
+  public ProductsController(ProductService productService) {
+    this.productService = productService;
+  }
+
+  @GetMapping("")
+  Iterable<Product> getAll() {
+    return productService.findAll();
+  }
+
+  @PostMapping("")
+  String postList(@RequestBody List<Product> data) {
+    productService.saveAll(data);
+    return "success";
+  }
+
 }
--- src/main/java/com/shopers/services/ProductService.java
+++ src/main/java/com/shopers/services/ProductService.java
@@ -1,4 +1,24 @@
 package com.shopers.services;
 
+import com.shopers.entities.Product;
+import com.shopers.repositories.ProductRepo;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ProductService {
+  private final ProductRepo productRepo;
+
+  public ProductService(ProductRepo productRepo) {
+    this.productRepo = productRepo;
+  }
+
+  public Iterable<Product> findAll() {
+    return productRepo.findAll();
+  }
+
+  public void saveAll(List<Product> data) {
+    productRepo.saveAll(data);
+  }
 }
