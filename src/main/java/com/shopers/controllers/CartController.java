4ce9b5d28c0f8da1c47a4986a6244131a4827305
ayxan386 <ayxanpro@gmail.com>
2019-12-20 18:33:59 +0400

    -- added GET endpoint -- added POST endpoint -- added PUT endpoint -- added DELETE endpoint -- added dto CartUpdate -- added dto CartDelete -- implemented needed service methods

--- src/main/java/com/shopers/controllers/CartController.java
+++ src/main/java/com/shopers/controllers/CartController.java
@@ -1,4 +1,39 @@
 package com.shopers.controllers;
 
+import com.shopers.dto.CartDelete;
+import com.shopers.dto.CartUpdate;
+import com.shopers.entities.Cart;
+import com.shopers.services.CartService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/cart**")
 public class CartController {
+  private final CartService cartService;
+
+  public CartController(CartService cartService) {
+    this.cartService = cartService;
+  }
+
+  @GetMapping("")
+  Iterable<Cart> getAll() {
+    return cartService.getAll();
+  }
+
+  @PostMapping("")
+  String addOne(@RequestBody Cart newPro) {
+    cartService.add(newPro);
+    return "success";
+  }
+
+  @PutMapping("")
+  String change(@RequestBody CartUpdate cartUpdate) {
+    cartService.change(cartUpdate);
+    return "success";
+  }
+
+  @DeleteMapping("")
+  String delete(@RequestBody CartDelete cart) {
+    return cartService.remove(cart);
+  }
 }
