d32c56db88903789654379e7d0f3169ced9e7848
Lala <hajiyeva.lalah@gmail.com>
2019-12-20 16:47:50 +0400

    -- cart class

--- src/main/java/com/shopers/entities/Cart.java
+++ src/main/java/com/shopers/entities/Cart.java
@@ -1,4 +1,60 @@
 package com.shopers.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Cart {
+    @Column(name = "id")
+    @Id
+    int id;
+
+    @Column(name = "count")
+    int count;
+
+    @Column(name = "owner")
+    int owner;
+
+    @Column(name = "status")
+    String status;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public int getOwner() {
+        return owner;
+    }
+
+    public void setOwner(int owner) {
+        this.owner = owner;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
