a48049770b5c7d8e652dfff0e41186ce68612ca1
ayxan386 <ayxanpro@gmail.com>
2019-12-22 17:51:49 +0400

    -- deployment

--- src/main/java/com/shopers/controllers/ProductController.java
+++ src/main/java/com/shopers/controllers/ProductController.java
@@ -19,10 +19,7 @@ public class ProductController {
 
     @GetMapping("/{id}")
     Optional<Product> getProduct(@PathVariable("id") int id){
-      System.out.printf("%s\n", id);
-      Optional<Product> byId = productService.getById(id);
-      byId.ifPresent(product -> System.out.println(product));
-      return byId;
+      return productService.getById(id);
     }
     @GetMapping("")
     Optional<Product> getProduct2(@RequestParam("id") int id){
